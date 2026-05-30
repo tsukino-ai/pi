4c51364a163be341c7316c5f0fb665fbdfdad7f3
iawb-ray <wangxingrui1997@gmail.com>
2026-05-30 12:57:08 +0800

    feat(webui): add ToolCallCard with dispatch to specialized renderers

---
+++ packages/webui/src/components/tool-renderers/index.ts
@@ -0,0 +1,5 @@
+export { ToolCallCard } from "./ToolCallCard.tsx";
+export { CodeBlock } from "./CodeBlock.tsx";
+export { DiffView } from "./DiffView.tsx";
+export { TerminalOutput } from "./TerminalOutput.tsx";
+export { FileNotice } from "./FileNotice.tsx";
